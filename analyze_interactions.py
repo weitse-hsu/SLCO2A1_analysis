87fa0e4946f077da746b7641ac6cfba0ae9f9e56
wehs7661 <wehs7661@colorado.edu>
2025-08-06 18:29:25 +0100

    Added analyze_interactions.py

---
+++ analyze_interactions.py
@@ -0,0 +1,212 @@
+import os
+import numpy as np
+import pandas as pd
+import MDAnalysis as mda
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import prolif as plf
+from prolif import fingerprint, molecule
+from MDAnalysis.analysis import contacts
+from matplotlib import rc
+
+def summarize_interactions(df, freq_threshold=0.5):
+    """
+    Given a DataFrame converted from interaction fingerprints, this function calculate the frequency of interactions
+    for those that occur in more than a specified percentage of the frames, and returns a summary DataFrame.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A MultiIndex DataFrame converted from interactoin fingerprints.
+    freq_threshold : float, optional
+        The minimum frequency threshold for interactions to be included in the summary. Default is 0.5.
+    
+    Returns
+    -------
+    final_df : pd.DataFrame
+        A DataFrame summarizing the interaction frequencies for each ligand-residue pair.
+    """
+    # 1. Simplify ligand and protein names by removing possible chain IDs, e.g. "LIG.A" -> "LIG"
+    new_ligand_levels = df.columns.levels[df.columns.names.index('ligand')].map(lambda x: x.split('.')[0])
+    new_protein_levels = df.columns.levels[df.columns.names.index('protein')].map(lambda x: x.split('.')[0])
+    df.columns = df.columns.set_levels(new_ligand_levels, level='ligand')
+    df.columns = df.columns.set_levels(new_protein_levels, level='protein')
+
+    # 2. Identify interactions that occur in more than freq_threshold fraction of frames
+    assert 0 < freq_threshold < 1, "Frequency threshold must be between 0 and 1."
+    freq_interactions = (
+        df.T.groupby(level=["ligand", "protein"])
+        .sum()
+        .T.astype(bool)
+        .mean()
+        .pipe(lambda s: s[s > freq_threshold].sort_values(ascending=False) * 100)
+        .to_frame(name="%")
+        .T
+        .squeeze()
+    )
+
+    # 3. Create a dictionary of interaction frequencies for each ligand-residue pair and convert it to a DataFrame
+    interaction_dict = {}
+    for ligand, residue in freq_interactions.index:
+        subset = df.xs(residue, level="protein", axis=1)
+        interaction_freq = subset.mean() * 100
+        interaction_dict[(ligand, residue)] = interaction_freq
+
+    summary_df = pd.DataFrame(interaction_dict).T
+    summary_df.index.names = ["Ligand", "Residue"]
+    summary_df = summary_df.fillna(0)
+
+    # 4. Add "Any interaction" column
+    ligands = freq_interactions.index.get_level_values("ligand").unique()  # should be only one ligand
+    any_interaction = freq_interactions.to_frame(name=(ligands[0], "Any interaction"))
+    any_interaction.columns = pd.MultiIndex.from_tuples(any_interaction.columns, names=["ligand", "interaction"])
+    full_df = pd.concat([summary_df, any_interaction], axis=1)
+
+    interaction_types = ["Any interaction"] + fingerprint.Fingerprint.list_available()
+    interaction_cols = pd.MultiIndex.from_product(
+        [ligands, interaction_types], names=["ligand", "interaction"]
+    )
+    interaction_cols = interaction_cols.intersection(full_df.columns)  # Get interactions occurring in the full_df
+    full_df = full_df[interaction_cols]
+
+    # 5. Format the DataFrame for easier readability
+    final_df = full_df.copy()
+    final_df.columns = final_df.columns.droplevel("ligand")
+    final_df.index = final_df.index.get_level_values(1)
+    final_df.index = final_df.index.str.title()
+    final_df.index = final_df.index.str.replace("^Hs[dep]", "His", regex=True)  # Replace Hsd, Hse, Hsp with His
+
+    rename_dict = {
+        "VdWContact": "VdW contact",
+        "HBAcceptor": "HB acceptor",
+        "HBDonor": "HB donor",
+        "CationPi": "Cation-Pi",
+        "PiCation": "Pi-cation",
+        "PiStacking": "Pi-stacking",
+        "EdgeToFace": "Edge-to-face",
+        "FaceToFace": "Face-to-face",
+        "MetalAcceptor": "Metal receptor",
+        "MetalDonor": "Metal donor",
+        "XBAcceptor": "XB acceptor",
+        "XBDonor": "XB donor",
+    }
+    final_df = final_df.rename(columns=rename_dict)
+
+    # Reorder the columns
+    interaction_order = [
+        "Any interaction", "Hydrophobic", "VdW contact", "HB acceptor", "HB donor", 
+        "Cation-Pi", "Pi-cation", "Pi-stacking", "Edge-to-face", "Face-to-face",
+        "Metal receptor", "Metal donor", "XB acceptor", "XB donor"
+    ]
+    available_columns = [col for col in interaction_order if col in final_df.columns]
+    final_df = final_df[available_columns]
+
+    return final_df
+
+
+def plot_interaction_frequencies(df, output_file):
+    """
+    Plots the interaction frequencies for a given DataFrame and saves the figure to the specified output file.
+    """
+    # Assign colors to different interaction types
+    interaction_types = fingerprint.Fingerprint.list_available()
+    colors = {i : plt.cm.tab20(i) for i in range(len(interaction_types))}
+    colors["Any interaction"] = "#0B84A5"
+    
+    # Overwrite a few with my favorites :)
+    colors.update({
+            "Hydrophobic": "lightblue",
+            "VdW contact": "#F6C85F",
+            "HB acceptor": "#CA472F",
+            "HB donor": "lightpink",
+        }
+    )
+
+    # Plot the grouped bar chart with custom colors
+    ax = df.plot(kind="bar", figsize=(14, 6), width=0.8, color=[colors[col] for col in df.columns])
+
+    # Labels
+    for label in ax.get_yticklabels():
+        label.set_fontproperties(fontprop)
+    for label in ax.get_xticklabels():
+        label.set_fontproperties(fontprop)
+
+    ax.set_ylabel("Frequency (%)", fontproperties=fontprop)
+    ax.tick_params(axis="y")
+    plt.xticks(rotation=45, ha="center", fontproperties=fontprop)
+    plt.legend(title="Interaction Type", bbox_to_anchor=(1.01, 1), loc="upper left", fontsize=18, title_fontsize=18, prop=fontprop)
+    plt.tight_layout()
+    ax.grid(axis='y')
+    plt.savefig(output_file, dpi=600, bbox_inches="tight")
+
+if __name__ == "__main__":
+    rc('font', **{
+        'family': 'sans-serif',
+        'sans-serif': ['DejaVu Sans'],
+        'size': 10
+    })
+    rc('mathtext', **{'default': 'regular'})
+    plt.rcParams['font.family'] = 'DejaVu Sans'
+    font_path = '/home/bioc1870/Software/mambaforge/envs/md_env/fonts/Arial.ttf'
+    fontprop = fm.FontProperties(fname=font_path, size=12)
+
+    systems = [
+        "RatSLCO2A1_P2E",
+        # "RatSLCO2A1_FEN",
+        # "RatSLCO2A1_TCW",
+        # "RatSLCO2A1_ZLK",
+        # "RatSLCO2A1_LSN",
+    ]
+
+    # We need to GRO files just to get the correct residue numbering
+    simulation_dir = "/home/bioc1870/SLCO2A1_simulations/"
+    gro_files = [f"{simulation_dir}{system}/production/rep_1/md.gro" for system in systems]
+    tpr_files = [f"{simulation_dir}{system}/production/rep_1/md_system.tpr" for system in systems]
+    xtc_files = [f"{simulation_dir}{system}/analysis/Clustering/md_all_center.xtc" for system in systems]
+
+    for gro_file, tpr_file, xtc_file, system in zip(gro_files, tpr_files, xtc_files, systems):
+        print(f"Processing {system} ...")
+        assert os.path.exists(gro_file), f"File {gro_file} does not exist."
+        assert os.path.exists(tpr_file), f"File {tpr_file} does not exist."
+        assert os.path.exists(xtc_file), f"File {xtc_file} does not exist."
+
+        ligand_name = system.split('_')[-1]
+        os.makedirs(f"results/{ligand_name}", exist_ok=True)
+
+        # Step 1. Load the MD trajectory into an MDAnalysis universe
+        u = mda.Universe(tpr_file, xtc_file)
+        u_ref = mda.Universe(gro_file)
+        u.residues.resids = u_ref.residues.resids[:len(u.residues.resids)]  # Fix the residue numbering in u
+        
+        # Step 2. Generate the IFP
+        if os.path.exists(f"results/{ligand_name}/{ligand_name}_ifp_all.pkl"):
+            print(f"IFP for {system} already exists, loading {ligand_name}_ifp_all.pkl ...")
+            fp = fingerprint.Fingerprint.from_pickle(f"results/{ligand_name}/{ligand_name}_ifp_all.pkl")
+            df = fp.to_dataframe()
+        else:
+            print(f"Starting IFP calculation for {system}...")
+            ligand_sel = u.select_atoms(f"resname {ligand_name}")
+            protein_sel = u.select_atoms("protein and byres around 20.0 group ligand", ligand=ligand_sel)
+            ligand_sel.chainIDs = np.array(['' for i in range(len(ligand_sel.chainIDs))])
+            protein_sel.chainIDs = np.array(['' for i in range(len(protein_sel.chainIDs))])
+
+            fp = fingerprint.Fingerprint()
+            fp.run(u.trajectory[::5], ligand_sel, protein_sel)  # 1 ns interval
+
+            print(f'Saving {ligand_name}_ifp_all.pkl and {ligand_name}_ifp_all.tsv...')
+            fp.to_pickle(f"results/{ligand_name}/{ligand_name}_ifp_all.pkl")
+
+            df = fp.to_dataframe()
+            df.to_csv(f"results/{ligand_name}/{ligand_name}_ifp_all.tsv", sep="\t", index=False)
+
+        # Step 3. Plot the interaction frequencies
+        print("Plotting interaction frequencies...")
+        final_df = summarize_interactions(df)
+        output_file = f"results/{ligand_name}/{ligand_name}_interaction_frequencies.png"
+        plot_interaction_frequencies(final_df, output_file)
+
+        # Step 4. Plot the interaction timeseries
+        print("Plotting interaction time series...")
+        fp.plot_barcode()
+        output_file = f"results/{ligand_name}/{ligand_name}_barcode.png"
+        plt.savefig(output_file, dpi=600, bbox_inches="tight")
